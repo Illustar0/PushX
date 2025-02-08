e73ce6c0f141e1740fd12b54fce35d86b5140c7c
Illustar0 <me@illustar0.com>
2025-02-08 19:38:31 +0800

    feat: support serverchanturbo fix: incorrect exception handling
    Signed-off-by: Illustar0 <me@illustar0.com>

--- pushx/providers/__init__.py
+++ pushx/providers/__init__.py
@@ -1,3 +1,4 @@
 from . import serverchan3 as ServerChan3
+from . import serverchanturbo as ServerChanTurbo
 
-__all__ = ["ServerChan3"]
+__all__ = ["ServerChan3", "ServerChanTurbo"]
--- pushx/providers/serverchanturbo.py
+++ pushx/providers/serverchanturbo.py
@@ -2,7 +2,7 @@ import json
 import httpx
 import logging
 from typing import Union
-from pydantic import Field, AliasChoices
+from pydantic import Field, AliasChoices, field_serializer
 from pushx.provider import ProviderMetadata, BasePushProvider, BaseProviderParams
 
 
@@ -11,37 +11,45 @@ logger.addHandler(logging.NullHandler())
 
 
 # Metadata
+# noinspection SpellCheckingInspection
 class NotifyParams(BaseProviderParams):
     title: str = Field(..., validation_alias=AliasChoices("title", "text"))
     """通知的标题"""
-    # noinspection SpellCheckingInspection
     desp: str = Field(None, validation_alias=AliasChoices("desp", "content", "message"))
     """通知的内容，支持 Markdown"""
     tags: str = None
     """通知的 tags"""
     short: str = None
     """通知的略缩"""
+    noip: bool = None
+    """是否隐藏调用 IP"""
+    channel: str = None
+    """使用的消息通道"""
+    openid: str = None
+    """消息抄送的 OPENID"""
+
+    @field_serializer("noip")
+    def serialize_noip(self, value: bool) -> int:
+        return int(value) if value is not None else None
 
 
+# noinspection SpellCheckingInspection
 class NotifierParams(BaseProviderParams):
-    # noinspection SpellCheckingInspection
     sendkey: str
-    """ServerChan3 的 SendKey"""
-    uid: int
-    """ServerChan3 的 UID"""
+    """ServerChanTurbo 的 SendKey"""
 
 
 __provider_meta__ = ProviderMetadata(
-    name="ServerChan3",
-    class_name="ServerChan3",
-    description="ServerChan3 Provider",
+    name="ServerChanTurbo",
+    class_name="ServerChanTurbo",
+    description="ServerChanTurbo Provider",
     notifier_params=NotifierParams,
     notify_params=NotifyParams,
     extra={},
 )
 
 
-class ServerChan3(BasePushProvider):
+class ServerChanTurbo(BasePushProvider):
     def _set_notifier_params(self, **kwargs: Union[NotifyParams, dict]):
         if isinstance(kwargs, dict):
             self._notifier_params = NotifierParams(**kwargs)
@@ -54,17 +62,15 @@ class ServerChan3(BasePushProvider):
         else:
             notify_params = kwargs
         response = httpx.post(
-            f"https://{self._notifier_params.uid}.push.ft07.com/send/{self._notifier_params.sendkey}.send",
+            f"https://sctapi.ftqq.com/{self._notifier_params.sendkey}.send",
             json=json.loads(notify_params.model_dump_json()),
         )
         try:
             if json.loads(response.text)["code"] != 0:
-                logger.error(f"ServerChan3 Push error, detail:{response.text}")
+                logger.error(f"ServerChanTurbo Push error, detail:{response.text}")
                 return False
             else:
                 return True
         except Exception as e:
-            logger.error(
-                f"ServerChan3 Push error, detail:{e}, response detail: {response.text}"
-            )
+            logger.error(f"ServerChanTurbo Push error, detail:{e}, response detail:")
             return False
