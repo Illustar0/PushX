06f7a97edc1499c93604ce18636b784c19a3f21d
Illustar0 <me@illustar0.com>
2025-02-09 14:41:48 +0800

    chore: bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, str(Path("../..", "pushx").resolve()))
 project = "PushX"
 copyright = "2025, Illustar0"
 author = "Illustar0"
-release = "0.1.2"
+release = "0.1.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
